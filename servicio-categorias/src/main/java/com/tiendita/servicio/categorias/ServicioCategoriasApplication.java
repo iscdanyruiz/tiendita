c7acd0185327f034d0448537935a8790036e0dff
Carlos Octavio Estrada Perez <carlos.estrada@forteinnovation.mx>
2022-07-27 20:09:19 -0500

    fix para el tomcat

--- servicio-categorias/src/main/java/com/tiendita/servicio/categorias/ServicioCategoriasApplication.java
+++ servicio-categorias/src/main/java/com/tiendita/servicio/categorias/ServicioCategoriasApplication.java
@@ -2,9 +2,10 @@ package com.tiendita.servicio.categorias;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ServicioCategoriasApplication {
+public class ServicioCategoriasApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServicioCategoriasApplication.class, args);
